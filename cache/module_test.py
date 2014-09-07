fecb181c7475b66be8600a13af6859b4232f041b
Johan Herland <johan@herland.net>
2014-09-07 02:43:27 +0200

    Add an initial test suite for module.py

---
+++ cache/module_test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2
+
+import sys
+import os
+import unittest
+from module import Module
+
+def test_path(path):
+    return os.path.join(os.path.dirname(sys.argv[0]), 'module_tests', path)
+
+class Test_SingleFile(unittest.TestCase):
+
+    def setUp(self):
+        self.root = Module.Root(test_path('single_file'))
+
+    def test_root_lookup(self):
+        self.assertEqual(self.root.foo, "bar")
+
+    def test_root_lookup_missing(self):
+        with self.assertRaises(AttributeError):
+            x = self.root.missing
+
+class Test_Submod(unittest.TestCase):
+
+    def setUp(self):
+        self.root = Module.Root(test_path('submod'))
+
+    def test_root_lookup(self):
+        self.assertEqual(self.root.foo, "bar")
+
+    def test_submod_lookup(self):
+        self.assertEqual(self.root.submod.foo, "baz")
+
+    def test_submod_lookup_missing(self):
+        with self.assertRaises(AttributeError):
+            x = self.root.missing_submod.foo
+
+class Test_Subdir(unittest.TestCase):
+
+    def setUp(self):
+        self.root = Module.Root(test_path('subdir'))
+
+    def test_root_lookup(self):
+        self.assertEqual(self.root.foo, "bar")
+
+    def test_subdir_lookup(self):
+        self.assertEqual(self.root.subdir.foo, "baz")
+
+    def test_subdir_lookup_missing(self):
+        with self.assertRaises(AttributeError):
+            x = self.root.missing_subdir.foo
+
+if __name__ == '__main__':
+    unittest.main()
